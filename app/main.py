fc0d2d669bdad2d3ceafa44ebfb1fbf7a3116899
Alex Nikitin <Nosify66@gmail.com>
2026-03-04 16:26:10 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -16,5 +16,8 @@ def copy_file(command: str) -> None:
     if not os.path.exists(source_file):
         return
 
-    with open(source_file, "r") as file_in, open(destination_file, "w") as file_out:
+    with (
+        open(source_file, "r") as file_in,
+        open(destination_file, "w") as file_out,
+    ):
         file_out.write(file_in.read())
